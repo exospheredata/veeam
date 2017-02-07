0ad3e00e318fb1d99944d69b5f44905228181a2e
Jeremy Goodrum <jermey@exospheredata.com>
2017-02-07 15:55:45 -0600

    Initial repository commit

---
+++ metadata.rb
@@ -0,0 +1,19 @@
+name 'veeam'
+maintainer 'Exosphere Data LLC'
+maintainer_email 'chef@exospheredata.com'
+license 'all_rights'
+description 'Installs/Configures Veeam Backup and Recovery'
+long_description 'Installs/Configures Veeam Backup and Recovery'
+version '0.1.0'
+
+supports 'windows'
+
+depends 'windows', '2.0.2'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/veeam/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/veeam' if respond_to?(:source_url)
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: veeam
+# Recipe:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
