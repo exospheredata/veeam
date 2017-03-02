ec1d81f3d91bf85cafcab025c9359fe7dab34443
Jeremy Goodrum <jermey@exospheredata.com>
2017-03-02 16:44:47 -0600

    UPDATE:  new node attributes for license_url and evaluation mode UPDATE:  Providers::Server to include an option to create the license file from a URL or DataBagItem UPDATE:  Recipes including veeam_server to pass the evaluation flag UPDATE:  Unit tests to stub_data_bag and fix the veeam_catalog_spec to stub .NET Framework.

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,3 +6,4 @@
 
 default['veeam']['installer']['package_url'] = 'http://download2.veeam.com/VeeamBackup&Replication_9.0.0.902.iso'
 default['veeam']['installer']['package_checksum'] = '21f9d2c318911e668511990b8bbd2800141a7764cc97a8b78d4c2200c1225c88'
+default['veeam']['license_url'] = nil
