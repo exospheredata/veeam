3e3bcc0345204010dbb81c3b812bf7e7af74c119
Jeremy Goodrum <jeremy@exospheredata.com>
2020-07-17 09:26:50 -0500

    FIX: Metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,14 +3,8 @@ maintainer 'Exosphere Data, LLC'
 maintainer_email 'chef@exospheredata.com'
 license 'Apache-2.0'
 description 'Installs/Configures Veeam Backup and Recovery'
-<<<<<<< HEAD
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '3.0.1'
-chef_version '>= 12.5' if respond_to?(:chef_version)
-=======
 version '4.0.0'
 chef_version '>= 13.0'
->>>>>>> Version 4.0.0 - Support for v10.0
 
 supports 'windows'
 
